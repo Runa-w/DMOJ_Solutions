6ea473fd274fcb72b7f92b6587a7c9f834eb9291
Runa-w <runa_w@gmx.com>
2022-10-10 06:08:01 -0500

    modified:   slot_machines.py

--- slot_machines.py
+++ slot_machines.py
@@ -4,7 +4,7 @@ second = int(input())
 third = int(input())
 
 plays = 0
-machine = 0
+
 
 while quarters >= 1:
     machine = plays % 3
@@ -14,11 +14,11 @@ if machine == 0:
     first = first + 1
     if first % 35 == 0:
         quarters = quarters + 30
-elif machine = 1:
+elif machine == 1:
     second = second + 1
     if second % 100 == 0:
         quarters = quarters + 60
-elif machine = 2:
+elif machine == 2:
     third = third + 1
     if third % 10 == 0:
         quarters = quarters + 9
